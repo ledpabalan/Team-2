fe196b7ca1a647d4cce9ec01b32fd7ac6caab130
slvcrl <slvcrl.monzales@gmail.com>
2021-11-18 20:30:57 +0800

    tnt-ui
    The New Tayuman

---
+++ application/header2.php
@@ -0,0 +1,68 @@
+<!-- header section starts  -->
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-utensils"></i> The New Tayuman </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#features">features</a>
+        <a href="#shops">shops</a>
+        <a href="#categories">categories</a>
+    </nav>
+
+    <div class="icons">
+        <div class="fas fa-bars" id="menu-btn"></div>
+        <div class="fas fa-search" id="search-btn"></div>
+        <div class="fas fa-shopping-cart" id="cart-btn"></div>
+        <div class="fas fa-user" id="login-btn"></div>
+    </div>
+
+    <form action="" class="search-form">
+        <input type="search" id="search-box" placeholder="search here...">
+        <label for="search-box" class="fas fa-search"></label>
+    </form>
+
+    <div class="shopping-cart">
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/cart-img-1.png" alt="">
+            <div class="content">
+                <h3>watermelon</h3>
+                <span class="price">$4.99/-</span>
+                <span class="quantity">qty : 1</span>
+            </div>
+        </div>
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/cart-img-2.png" alt="">
+            <div class="content">
+                <h3>onion</h3>
+                <span class="price">$4.99/-</span>
+                <span class="quantity">qty : 1</span>
+            </div>
+        </div>
+        <div class="box">
+            <i class="fas fa-trash"></i>
+            <img src="image/cart-img-3.png" alt="">
+            <div class="content">
+                <h3>chicken</h3>
+                <span class="price">$4.99/-</span>
+                <span class="quantity">qty : 1</span>
+            </div>
+        </div>
+        <div class="total"> total : $19.69/- </div>
+        <a href="#" class="btn">checkout</a>
+    </div>
+
+    <form action="" class="login-form">
+
+            <a href="profile.php">Profile</a><br>
+            <a href="index.php">Log out</a><br>
+            <a href="settings.php">Settings</a>
+
+    </form>
+
+</header>
+
+<!-- header section ends -->
---
+++ application/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <?php include 'headlog.php'; ?>
+
+</head>
+<body>
+
+    <!-- login start-->
+    <div class="loginbox">
+        <img class="logo" src="../images/TNTlogo.png">
+        <img class="avatar" src="../images/login.png">
+            <h1>LOG IN</h1>
+            <form>
+                <input type="text" name="" placeholder=" Username/Email">
+
+                <input type="text" name="" placeholder=" Password">
+
+                <a href="homepage.php"><i class="fas fa-chevron-circle-right"></i></a>
+                
+                <h5>Don't have any account?</h5>
+                <a href="signup.php">Sign up</a>
+
+            </form>
+    </div>
+    <!-- login end-->
+
+
+</body>
+</html>
