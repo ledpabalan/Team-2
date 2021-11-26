f1d9dc406bbed05030aacd7c62eecf44325be5d2
Jairus Ivan Iscala <91131095+IscalaJai27@users.noreply.github.com>
2021-11-26 08:04:22 +0800

    Few Changes
     -Dinelete si welcome.php at pinalitan ng users.php as defaullt controller
    -change default controller sa route since pinalitan si  welcome.php into users.php as defaullt controller
    -Problem: Di matawag si register function sa Users Controller

--- application/controllers/Users.php
+++ application/controllers/Users.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Users extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -20,6 +20,13 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+      $this->load->view('welcome_message');
 	}
+
+	public function register() 
+	{
+        echo 'REGISTER ITO';
+        exit;
+    }
+
 }
