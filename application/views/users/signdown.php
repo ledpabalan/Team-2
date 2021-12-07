c7fce7da5d8d0d207402517b7c6678d5e5ac7b7f
Levi <levituya08@gmail.com>
2021-12-07 19:17:17 +0800

    signdown changes

--- application/views/users/signdown.php
+++ application/views/users/signdown.php
@@ -33,44 +33,11 @@
                 <p>Username:</p>
                 <input type="text" name="user_username" placeholder=" Enter Username"required>
 
-                <p>Contact Number:</p>
-                <input type="text" name="user_contact_no" placeholder=" Enter Contact No."required>
-
-                <p>Birthdate:</p>
-                <input type="date" name="user_birthday" placeholder=" DD-MM-YYYY"required>
-
-                <p>Gender:</p>
-                <input type="radio" id="Gender" name="user_gender" value="Male"/>Male
-                <input type="radio" id="Gender" name="user_gender" value="Female"/>Female
-                <input type="radio" id="Gender" name="user_gender"value="Other"/>Other
-
                 <p>Password:</p>
                 <input type="password" name="user_password" placeholder=" Enter Password"required>
 
                 <p>Confirm Password:</p>
                 <input type="password" name="user_pwdRepeat" placeholder=" Confirm Password"required>
-        
-                <p>Verification:</p>
-                <input type="file" id="real-file" hidden="hidden" />
-                <button type="button" id="custom-button">CHOOSE A FILE</button>
-                <span id="custom-text">UPLOAD VERIFICATION</span>
-
-                <script>
-                    const realFileBtn = document.getElementById("real-file");
-                    const customBtn = document.getElementById("custom-button");
-                    const customTxt = document.getElementById("custom-text");
-
-                    customBtn.addEventListener("click", function() {
-                        realFileBtn.click();
-                    });
-
-                    realFileBtn.addEventListener("change", function() {
-                        if (realFileBtn.value) {
-                            customTxt.innerHTML = realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
-                        }else {
-                            customTxt.innerHTML = "No file chosen, yet.";
-                        }
-                    });
 
                 </script>
                 <!-- <a href="<?php echo base_url(); ?>"type= "submit"><i class="fas fa-chevron-circle-right"></i></a> -->
