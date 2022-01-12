a60624bcbc47c5393ae7cd7a8beaed55f7820344
Jairus Ivan Iscala <91131095+IscalaJai27@users.noreply.github.com>
2022-01-12 21:38:07 +0800

    sellerside interface
    basic homepage section for sellerside

---
+++ application/views/users/sellerside/shopsec.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<?php include 'assets/headsellerside.php'; ?>
+
+</head>
+<body>
+<?php include 'assets/header2sellerside.php'; ?>  
+
+<!-- shops section starts  -->
+
+<section class="shops" id="shops">
+
+    <h1 class="heading"> our <span>shops</span> </h1>
+    <h1 class="heading"> Seller-Side </h1>
+
+    <div class="swiper shop-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="swiper shop-slider">
+
+        <div class="swiper-wrapper">
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+            <div class="swiper-slide box">
+                <img src="<?php echo base_url(); ?>assets/images/shop logo.png" alt="">
+                <h3>Shop Name</h3>
+                <div class="price"> Starts at ₱4.99- </div>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+                </div>
+                <a href='<?php echo base_url()."users/usershop"?>' class="btn">Check now</a>
+            </div>
+
+        </div>
+
+    </div>
+
+
+</section>
+
+<!-- shops section ends -->
+
+
+
+<?php include 'assets/footer.php'; ?>  
+
+<script src="<?php echo base_url(); ?>  https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="<?php echo base_url(); ?>assets/script.js"></script>
+
+</body>
+</html>
