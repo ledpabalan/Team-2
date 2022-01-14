cb4275faeda133ff71ea1761ca1977d9fcf98fe3
Louise <lxedpabalan@gmail.com>
2022-01-14 13:31:15 +0800

    update

--- application/controllers/Product.php
+++ application/controllers/Product.php
@@ -64,7 +64,7 @@ public function viewprod()
 		//  }
 
          $this->load->model('vendor_model');  
-		 $item=$this->vendor_model->Getitem(); //dito ipapasa yung target item
+		 $item=$this->vendor_model->Getitem(14); //dito ipapasa yung target item
 		 print_r($item);
 		 $this->load->view('vendor/editprod1', $item);
 		 }
