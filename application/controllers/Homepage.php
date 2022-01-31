09cdce110739c720e1642db521af441958a00ff3
Jairus Ivan Iscala <91131095+IscalaJai27@users.noreply.github.com>
2022-02-01 02:21:47 +0800

    delete unneccessaries

--- application/controllers/Homepage.php
+++ application/controllers/Homepage.php
@@ -3,7 +3,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Homepage extends CI_Controller {
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////
 
     public function __construct()
     {
@@ -18,7 +17,7 @@ class Homepage extends CI_Controller {
     public function index()
 	{
         redirect('/logout');
-    // $this->load->view('homepage_index');
+    
 	}
 
 
