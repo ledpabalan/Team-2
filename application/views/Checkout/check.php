0cac3e1bb1d9066b9ce47e69cc6c77413ebafefd
John Edward D. Complido <91130909+Drawde0731@users.noreply.github.com>
2022-02-08 00:59:28 +0800

    fixing (4/infinity)

--- application/views/Checkout/check.php
+++ application/views/Checkout/check.php
@@ -1,7 +1,14 @@
-<?=isset($message) ? $message : "";?>
+<?= isset($message) ? $message : ""; ?>
 <!DOCTYPE html>
 <html>
-<head></head>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title>Product Add - Seller Side Interface</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+    <?php include 'assets/headbuyerside.php'; ?>
+</head>
+
 <body>
     <?php include 'assets/header2buyerside.php'; ?>
 
@@ -36,8 +43,64 @@
 
                 <br> <br>
 
-</body>
-</html>
-   
-    
+                <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-check"></span> Submit</button>
+            </form>
+        </div>
+    </div>
+    <?php include 'assets/footer.php'; ?>  
 
+<script src="<?php echo base_url(); ?>https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="<?php echo base_url(); ?>assets/script.js"></script>
+
+<script>
+    /*script for nav bar */
+    let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+</script>
+</body>
+
+
+</html>
