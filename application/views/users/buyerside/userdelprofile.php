1898d22b4ac9a63836c16505c1354f83c6568735
John Edward D. Complido <91130909+Drawde0731@users.noreply.github.com>
2022-02-08 18:14:44 +0800

    deactwork

--- application/views/users/buyerside/userdelprofile.php
+++ application/views/users/buyerside/userdelprofile.php
@@ -20,15 +20,14 @@
             <a class="lobtn" href= '<?php echo base_url()."/logout"?>'>LOG OUT</a>
 
         <div class="deactbox">
+        <form method="POST" action="/Team-2/Usersbuyerside/updatestatus" >  
             <h3>Deactivate Account?</h3>
-
-            <h4>Confirm Password: <input type="text" name="confirmpassword" placeholder=" Confirm Password"></h4>
+            <h4>Confirm Password: <input type="password" name="user_pwdRepeat" placeholder=" Confirm Password" Required></h4>
             <h4>Reasons for deactivating: <br> <textarea name="message" rows="10" placeholder="Your Reason for deactivating" required></textarea></h4>
            
-            <form method="POST" action="/Team-2/Usersbuyerside/updatestatus">      
-            <input type="submit" value="DEACTIVATE" class="dbtn">
 
-        
+                
+            <input type="submit" value="DEACTIVATE" class="dbtn" >
         </div>
 
     </div>
